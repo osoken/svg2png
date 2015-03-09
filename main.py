287391ea1644fe2c8458614511c0c916c88ae088
osoken <osoken.devel@outlook.jp>
2015-03-10 00:49:24 +0900

    modified:   main.py

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
+from flask import Flask,request,Response,make_response;
+from wand.image import Image;
 
-from wand.api import library
-import wand.color
-import wand.image
+app = Flask(__name__)
 
-svg_file = file('test.svg');
-output_file_name = 'test_out.png';
+@app.route('/', methods=['GET','POST'])
+def convert_svg2png():
+  if request.method=='POST':
+    try:
+      with Image(blob=str(request.form['svg']), format='svg') as image:
+        image.read();
+        png_image = image.make_blob('png32');
+      response = make_response(png_image)
+      response.headers['Content-Type'] = 'image/png'
+      return response;
+    except Exception as e:
+      return Response(response=str(e), status=400);
+  return '''
+      <html><body><form action='/' method='POST'><textarea name=svg></textarea><input type='submit'></form></body></html>
+    '''
 
-with wand.image.Image() as image:
-    with wand.color.Color('transparent') as background_color:
-        library.MagickSetBackgroundColor(image.wand, 
-                                         background_color.resource) 
-    image.read(blob=svg_file.read(), "svg")
-    png_image = image.make_blob("png32")
-
-with open(output_filename, "wb") as out:
-    out.write(png_image)
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080);
